b0de4de930b3de92c43ef640732a88084a8fdf2b
williandiniz <williandiniz@gmail.com>
2022-02-14 12:28:30 -0300

    Add info html

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ echo "<h1>Olá Mundo!</h1>";
 echo "<p>";
 echo "<h2>Testando novo build</h2>";
 echo "<p>";
-echo "<h3>Novamente realizando um build</h3>";
+echo "<h3>Novamente realizando um build...</h3>";
 
 ?>
 <html>
